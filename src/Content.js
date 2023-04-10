f68e1626a97cac458d9c7949dc2a7178f46cfaf0
makhmudoff <gachon031698@gmail.com>
2023-04-10 23:36:22 +0900

    Added Content section

---
+++ src/Content.js
@@ -0,0 +1,18 @@
+import ItemList from "./ItemList";
+const Content = ({ items, handleCheck, handleDelete }) => {
+  return (
+    <main>
+      {items.length ? (
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ) : (
+        <p style={{ marginTop: "2rem" }}>Your list empty.</p>
+      )}
+    </main>
+  );
+};
+
+export default Content;
